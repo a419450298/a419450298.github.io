598d03d131db0f29da5fa20f7c67cce1a30eb8fa
Stone <49929299+a419450298@users.noreply.github.com>
2024-12-30 00:54:56 +0800

    Site updated: 2024-12-30 00:54:55

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/29/写给2025的一封信/"];function toRandomPost(){
+var posts=["2024/12/29/index/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
