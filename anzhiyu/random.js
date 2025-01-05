12606fdabc96a659086e0c26540276ed72dcff2d
Stone <49929299+a419450298@users.noreply.github.com>
2025-01-05 08:46:09 +0800

    Site updated: 2025-01-05 08:46:03

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/29/写给2024的一封信/","2025/01/01/搭建samba服务实现文件共享/"];function toRandomPost(){
+var posts=["2025/01/01/搭建samba服务实现文件共享/","2025/01/05/wireshark从入门到实战/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
